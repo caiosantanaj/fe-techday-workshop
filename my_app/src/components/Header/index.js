b51ba2c122c6324eec064c72dad55df11a78b5ab
Caio Jacobina <caiosantanaj@gmail.com>
2018-06-18 02:40:03 +0100

    some updates

--- my_app/src/components/Header/index.js
+++ my_app/src/components/Header/index.js
@@ -1,21 +1,24 @@
 import React  from 'react';
 
-import bradPittPhoto from './brad-pitt.png';
+import caioPhoto from './mypphoto.jpeg';
 import './styles.css';
 
 
 const Header = () => (
   <header>
     <div className={"header-container"}>
-      <img src={bradPittPhoto} alt={""} className="header-photo"/>
-      <h1>Brad Pitt</h1>
-      <p>Hi there, I'm a passionate front-end developer who loves to build beautiful interfaces using the most
-        modern web technologies. This portfolio for example, is built using React and GraphQL, these are just some
-        of the technologies I love! I'm currently looking for an opportunity in Porto, give me a call</p>
+      <img src={caioPhoto} alt={""} className="header-photo"/>
+      <h1>Caio Jacobina</h1>
+      <p>Hi, my name is Caio Jacobina and I'm a Student at University of Aveiro.
+        <br/>
+        This portfolio was made in Mindera Frontend Tech Day using React and GraphQL.
+        <br/>
+        These are just two of the technologies that I have been learning! 
+        I'm currently looking for an opportunity in Aveiro/Porto, give me a call!</p>
       <div className={"header-social-container"}>
-        <a href={"https://twitter.com/i/flow/consent_violation_flow"} target="_blank"><i className="fab fa-twitter"/> Twitter</a>
-        <a href={"https://www.facebook.com/actorbradpitt"} target="_blank"><i className="fab fa-facebook-f"/> Facebook</a>
-        <a href={"https://www.linkedin.com/in/brad-pitt-6236b5151/"} target="_blank"><i className="fab fa-linkedin-in"/> LinkedIn</a>
+        <a href={"https://www.facebook.com/caiosantanaj"} target="_blank"><i className="fab fa-facebook-f"/> Facebook</a>
+        <a href={"https://www.linkedin.com/in/caiosantanaj"} target="_blank"><i className="fab fa-linkedin-in"/> LinkedIn</a>
+        <a href={"https://github.com/caiosantanaj/"} target="_blank"><i className="fab fa-github"/> GitHub</a>
       </div>
     </div>
   </header>
